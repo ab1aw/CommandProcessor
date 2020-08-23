58a565b0b7bdb37d00e0eaec41528d551b4d72a5
Polia, Michael (US) <Michael.Polia@flir.com>
2020-08-23 09:23:52 -0400

    Renamed classes to designate command parser and command options.

--- main.cpp
+++ main.cpp
@@ -11,95 +11,95 @@ public:
       // Create a event that will be performed.. doesn't need to be in this class can be defined anywhere.
       EventManager *myEventManager = EventManager::Instance();
 
-      printf("createEvent(testEvent-B1a)\n");
-      myEventManager->createEvent("testEvent-B1a");
+      printf("createEvent(testEvent-COptionOne)\n");
+      myEventManager->createEvent("testEvent-COptionOne");
 
-      printf("createEvent(testEvent-B1b)\n");
-      myEventManager->createEvent("testEvent-B1b");
+      printf("createEvent(testEvent-COptionTwo)\n");
+      myEventManager->createEvent("testEvent-COptionTwo");
     };
   };
 
   A() {
     // Create a event that will be performed.. doesn't need to be in this class can be defined anywhere.
     EventManager *myEventManager = EventManager::Instance();
-    printf("createEvent(testEvent-B)\n");
-    myEventManager->createEvent("testEvent-B");
+    printf("createEvent(testEvent-CCommandParser)\n");
+    myEventManager->createEvent("testEvent-CCommandParser");
   }
   
   void fireEvents() {
     // Fire the event and all the subscribed class methods will get called.
     EventManager *myEventManager = EventManager::Instance();
-    myEventManager->execute("testEvent-B");
-    myEventManager->execute("testEvent-B1a");
-    myEventManager->execute("testEvent-B1b");
+    myEventManager->execute("testEvent-CCommandParser");
+    myEventManager->execute("testEvent-COptionOne");
+    myEventManager->execute("testEvent-COptionTwo");
   }
 
   A1 MyA1;
 };
 
-class B {
+class CCommandParser {
 public:
 
-  class CSomeBaseClass
+  class CGenericOption
   {
   };
   
-  class B1a : CSomeBaseClass
+  class COptionOne : CGenericOption
   {
     public:
-    void listenerB1a() {
-      printf("Listener of B1a called\n");
+    void listenerCOptionOne() {
+      printf("Listener of COptionOne called\n");
     };
 
     void attachToEvent() {
       // When testEvent is called the listener method of this class will get called.
       EventManager *myEventManager = EventManager::Instance();
 
-      printf("subscribe(testEvent-B1a)\n");
-      myEventManager->subscribe("testEvent-B1a", this, &B::B1a::listenerB1a);
+      printf("subscribe(testEvent-COptionOne)\n");
+      myEventManager->subscribe("testEvent-COptionOne", this, &CCommandParser::COptionOne::listenerCOptionOne);
     }
   };
 
 
-  class B1b : CSomeBaseClass
+  class COptionTwo : CGenericOption
   {
     public:
-    void listenerB1b() {
-      printf("Listener of B1b called\n");
+    void listenerCOptionTwo() {
+      printf("Listener of COptionTwo called\n");
     };
 
     void attachToEvent() {
       // When testEvent is called the listener method of this class will get called.
       EventManager *myEventManager = EventManager::Instance();
 
-      printf("subscribe(testEvent-B1b)\n");
-      myEventManager->subscribe("testEvent-B1b", this, &B::B1b::listenerB1b);
+      printf("subscribe(testEvent-COptionTwo)\n");
+      myEventManager->subscribe("testEvent-COptionTwo", this, &CCommandParser::COptionTwo::listenerCOptionTwo);
     }
   };
   
   
-  void listenerB() {
-    printf("Listener of B called\n");
+  void listenerCCommandParser() {
+    printf("Listener of CCommandParser called\n");
   }
   
   void attachToEvent() {
     // When testEvent is called the listener method of this class will get called.
     EventManager *myEventManager = EventManager::Instance();
 
-    printf("subscribe(testEvent-B)\n");
-    myEventManager->subscribe("testEvent-B", this, &B::listenerB);
+    printf("subscribe(testEvent-CCommandParser)\n");
+    myEventManager->subscribe("testEvent-CCommandParser", this, &CCommandParser::listenerCCommandParser);
     
-    MyB1a.attachToEvent();
-    MyB1b.attachToEvent();
+    MyCOptionOne.attachToEvent();
+    MyCOptionTwo.attachToEvent();
   }
 
-  B1a MyB1a;
-  B1b MyB1b;
+  COptionOne MyCOptionOne;
+  COptionTwo MyCOptionTwo;
 };
 
 int main() {
   A *obj0 = new A;
-  B *obj1 = new B;
+  CCommandParser *obj1 = new CCommandParser;
   
   obj1->attachToEvent();
   obj0->fireEvents();
